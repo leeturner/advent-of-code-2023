63dfd2bfe8d86a3c38ffc78fc1c907217b31c148
leeturner <lee@leeturner.me>
2023-12-01 09:55:32 +0000

    Solution to day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,42 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+  fun List<String>.solve(): Int {
+    return this.sumOf { line ->
+      val first = line.first { it.isDigit() }
+      val last = line.last { it.isDigit() }
+      "$first$last".toInt()
     }
+  }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+  fun part1(input: List<String>): Int {
+    return input.solve()
+  }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+  val replacements = listOf(
+    "one" to 1,
+    "two" to 2,
+    "three" to 3,
+    "four" to 4,
+    "five" to 5,
+    "six" to 6,
+    "seven" to 7,
+    "eight" to 8,
+    "nine" to 9,
+  )
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+  fun part2(input: List<String>): Int {
+    return input.map { replacements.fold(it) { line, (key, value) -> line.replace(key, "$key$value$key") } }.solve()
+  }
+
+  val testInputPart1 = readInput("Day01Part1_test")
+  println(part1(testInputPart1))
+  check(part1(testInputPart1) == 142)
+
+  val testInputPart2 = readInput("Day01Part2_test")
+  println(part2(testInputPart2))
+  check(part2(testInputPart2) == 281)
+
+  val input = readInput("Day01")
+  part1(input).println()
+  part2(input).println()
 }
